df1ed7f9ec0f9165429b113d056bffece8ff4b79
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-02-14 12:13:12 +0100

    added argument test and benchmark

--- randutil/randutil_test.go
+++ randutil/randutil_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+func BenchmarkInt64(b *testing.B) {
+	r := rand.New(NewTimeSeed())
+	for i := 0; i < b.N; i++ {
+		Int64(r, 10, 100)
+	}
+}
+
+func TestArgumentsInt64(t *testing.T) {
+	_, err := Int64(nil, 10, 100)
+	if err == nil {
+		t.Fatalf("With nil as Rand struct, Int64() needs to return (_, nil)")
+	}
+
+	_, err = Int64(rand.New(NewTimeSeed()), 10, 5)
+	if err == nil {
+		t.Fatalf("With min > max as arguments, Int64() needs to return (_, nil)")
+	}
+}
+
 func TestGetInt64(t *testing.T) {
 	var min int64 = 10
 	var max int64 = 100
