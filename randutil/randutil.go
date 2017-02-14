4b49d3b99165bea605249d668f93ab1269c60832
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-02-14 12:12:42 +0100

    added argument check and docs

--- randutil/randutil.go
+++ randutil/randutil.go
@@ -1,8 +1,20 @@
 package randutil
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
+// Int64 generate a random number (min >= x > max) with *rand.Rand given.
+// if r is nil or min > max, this function return an error.
 func Int64(r *rand.Rand, min, max int64) (int64, error) {
-	// TODO do checks
+	if r == nil {
+		return -1, fmt.Errorf("Random object given is nil")
+	}
+
+	if min > max {
+		return -1, fmt.Errorf("Bounds error: %d > %d", min, max)
+	}
+
 	return r.Int63n(max-min) + min, nil
 }
