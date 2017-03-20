ea1a43a635a0e0473528ee75b6cace581b1ace5f
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-03-20 14:17:05 +0100

    removed new method to write slice to file and tests

--- out/out.go
+++ out/out.go
@@ -1,33 +1,10 @@
 package out
 
 import (
-	"bytes"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"os"
 )
-
-// utility function to convert a slice of int64 into a slice of bytes.
-// return (nil, error) if slice is nil or if writing int64 in the buffer rise
-// an error.
-func convert(slice []int64) ([]byte, error) {
-	if slice == nil {
-		return nil, errors.New("Slice given can not be null")
-	}
-
-	// make a buffer of  bytes
-	buf := new(bytes.Buffer)
-	// for every int64 into slice, convert it in byte and push it in the buffer
-	for _, e := range slice {
-		if err := binary.Write(buf, binary.LittleEndian, e); err != nil {
-			return nil, err
-		}
-	}
-
-	// Bytes() function return a []byte
-	return buf.Bytes(), nil
-}
 
 // utility function to check if open file given is a directory or not.
 func isDirectory(file *os.File) (error, bool) {
@@ -63,58 +40,46 @@ func openIfCanRW(file string) (*os.File, error) {
 	return openFile, nil
 }
 
-// this utiliy function writes the header of the file according to structure
-// given. The headers for each data structure are described in README.
-func writeHeader(structure interface{}, path string) error {
-}
-
-func WriteA(slice []int64, path string) error {
-	return nil
+// reusable method to write a single slice on given file
+func writeSingleSlice(slice []int64, file *os.File) error {
+	var s string
+	for _, v := range slice {
+		s = fmt.Sprintf("%d ", v)
+		if _, err := file.WriteString(s); err != nil {
+			return err
+		}
+	}
+	_, err := file.WriteString("\n")
+	return err
 }
 
 // TODO add doc
 func Write(slice []int64, path string) error {
-	// TODO check args
-	// TODO !!! check if file is not a binary file || directory || permissions
+	if slice == nil {
+		return errors.New("Given slice can not be nil")
+	}
 
-	file, err := os.Create(path)
-	if err != nil {
+	var file *os.File
+	if file, err := openIfCanRW(path); err != nil {
 		return err
 	}
 	defer file.Close()
 
 	// create and write file header
 	s := fmt.Sprintf("%d\n", len(slice))
-	_, err = file.WriteString(s)
-	if err != nil {
+	if _, err = file.WriteString(s); err != nil {
 		return err
 	}
 
 	// create and write file body
-	err = writeSingleSlice(slice, file)
-	if err != nil {
+	if err := writeSingleSlice(slice, file); err != nil {
 		return err
 	}
 
 	// Issue a Sync to flush writes to stable storage
-	err = file.Sync()
-	if err != nil {
+	if err := file.Sync(); err != nil {
 		return err
 	}
 
 	return nil
-}
-
-// reusable method to write a single slice on given file
-func writeSingleSlice(slice []int64, file *os.File) error {
-	var s string
-	for _, v := range slice {
-		s = fmt.Sprintf("%d ", v)
-		_, err := file.WriteString(s)
-		if err != nil {
-			return err
-		}
-	}
-	_, err := file.WriteString("\n")
-	return err
 }
--- out/out_internal_test.go
+++ out/out_internal_test.go
@@ -5,30 +5,7 @@ import (
 	"testing"
 )
 
-func TestConvert(t *testing.T) {
-	slices := [][]int64{
-		{1, 2, 3, 4},
-		{10, 9, 8, 7},
-		{-1, -2, -3, -4},
-		{-10, -9, -8, -7},
-		{1, -2, 3, -4},
-		{-10, -9, 8, 7},
-	}
-
-	for _, e := range slices {
-		byteSlice, err := convert(e)
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Logf("% x", byteSlice)
-	}
-}
-
-func TestArgumentConvert(t *testing.T) {
-	if _, err := convert(nil); err != nil {
-		t.Fatal("convert(nil) must return an error")
-	}
-}
+// TODO add test for isDirectory
 
 func TestArgumentsCheckRW(t *testing.T) {
 	noRead := "no-read.txt"
