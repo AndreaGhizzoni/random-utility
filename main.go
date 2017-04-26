378a18af968d4719790b941abbf06fee2bc72331
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-04-26 17:07:08 +0200

    rewrite main with urfave/cli library

--- main.go
+++ main.go
@@ -1,100 +1,128 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/AndreaGhizzoni/zenium/samples"
+	"github.com/urfave/cli"
 	"os"
+	"sort"
 )
-
-var (
-	// this var is used for '-generate|-g xxx' flag, where xxx is:
-	//  - rslice
-	//  - oslice
-	//  - matrix
-	//  - bound
-	sampleGeneration string
-
-	// this var is used for '-output|-o xxx' flag, where xxx is by default
-	// std out or:
-	//  - path/to/file
-	out string
-
-	// those two flags are used for '-min|-m' and '-max|-M' bounds respectively
-	m, M int64
-
-	// those two flags are used for '--row|-r' and '--col|-c' in matrix
-	// generation. cols is also used when `--length|-l` is set in slice
-	// generation.
-	cols, rows int64
-)
-
-func init() {
-	flag.StringVar(&sampleGeneration, "generate", "", generateUsage)
-	flag.StringVar(&sampleGeneration, "g", "", generateUsage)
-
-	flag.StringVar(&out, "output", "", outUsage)
-	flag.StringVar(&out, "o", "", outUsage)
-
-	flag.Int64Var(&m, "min", -922337203685477580, minUsage)
-	flag.Int64Var(&m, "m", -922337203685477580, minUsage)
-	flag.Int64Var(&M, "max", 922337203685477580, maxUsage)
-	flag.Int64Var(&M, "M", 922337203685477580, maxUsage)
-
-	flag.Int64Var(&cols, "columns", 0, colsUsage)
-	flag.Int64Var(&cols, "c", 0, colsUsage)
-	flag.Int64Var(&cols, "length", 0, lengthUsage)
-	flag.Int64Var(&cols, "l", 0, lengthUsage)
-
-	flag.Int64Var(&rows, "rows", 0, rowsUsage)
-	flag.Int64Var(&rows, "r", 0, rowsUsage)
-}
-
-// utility function to print a message and helper, than exit 1
-func printDefaults(msg string) {
-	if msg != "" {
-		fmt.Println(msg)
-	}
-	flag.PrintDefaults()
-	os.Exit(1)
-}
 
 func main() {
-	flag.Parse()
-	// no flag set, print defaults
-	if flag.NFlag() == 0 {
-		printDefaults(fmt.Sprintf(helpHeader, os.Args[0]))
+	app := cli.NewApp()
+	app.Name = "zenium"             // TODO move in separate file
+	app.Version = "app.Version"     // TODO move in separate file
+	app.Usage = "app.Usage"         // TODO move in separate file
+	app.UsageText = "app.UsageText" // TODO move in separate file
+	// TODO move in separate file
+	app.ArgsUsage = "generate [rslice|oslice|matrix|bound]"
+	app.Authors = []cli.Author{
+		{Name: "Andrea Ghizzoni", Email: "andrea.ghz@gmail.com"},
 	}
 
-	// missing -g|-generate cli flag
-	if sampleGeneration == "" {
-		printDefaults("What to generate missinig: check --generate|-g " +
-			"argument.")
+	// TODO maybe move all flags to separate file
+	outFlag := cli.StringFlag{
+		Name:  "out, o",
+		Value: "",
+		Usage: "usage flag out `FILE`",
+	}
+	minFlag := cli.Int64Flag{
+		Name:  "min, m",
+		Value: -922337203685477580,
+		Usage: "usage flag min",
+	}
+	maxFlag := cli.Int64Flag{
+		Name:  "max, M",
+		Value: 922337203685477580,
+		Usage: "usage flag max",
+	}
+	columnFlag := cli.Int64Flag{
+		Name:  "columns, c",
+		Value: 0,
+		Usage: "usage flag column",
+	}
+	rowsFlag := cli.Int64Flag{
+		Name:  "rows, r",
+		Value: 0,
+		Usage: "usage flag rows",
+	}
+	lengthFlag := cli.Int64Flag{
+		Name:  "length, l",
+		Value: 0,
+		Usage: "usage flag length",
 	}
 
-	gen := samples.NewGenerator()
-	// check if -g|-generate flag has an accepted value
-	switch sampleGeneration {
-	case "rslice":
-		s, err := gen.Slice(cols, m, M)
-		if err != nil {
-			printDefaults(err.Error())
-		}
-		fmt.Println(s) // TODO change this when output package is ready
-	case "oslice":
-		// TODO change this when output package is ready
-		fmt.Println("Not implemented yet :P")
-	case "matrix":
-		m, err := gen.Matrix(rows, cols, m, M)
-		if err != nil {
-			printDefaults(err.Error())
-		}
-		fmt.Println(m) // TODO change this when output package is ready
-	case "bound":
-		// TODO change this when output package is ready
-		fmt.Println("Not implemented yet :P")
-	default:
-		printDefaults("What to generate not recognized. Check --generate|-g " +
-			"argument.")
+	app.Commands = []cli.Command{
+		{
+			Name:    "generate",
+			Aliases: []string{"g"},
+			//Category:    "generate command",
+			Usage: "generate usage",
+			//UsageText:   "generate usage text",
+			Description: "generate description",
+			ArgsUsage:   "generate args usage",
+			Subcommands: cli.Commands{
+				cli.Command{
+					Name:  "rslice",
+					Usage: "rslice usage",
+					Flags: []cli.Flag{outFlag, minFlag, maxFlag, lengthFlag},
+					Action: func(c *cli.Context) error {
+						fmt.Println("generate rslice command")
+						fmt.Printf("out flag is set ? %v\n", c.IsSet("out"))
+						fmt.Printf("min flag is set ? %v\n", c.IsSet("min"))
+						fmt.Printf("max flag is set ? %v\n", c.IsSet("max"))
+						fmt.Printf("length flag is set ? %v\n",
+							c.IsSet("length"))
+						return nil
+					},
+				},
+				cli.Command{
+					Name:  "oslice",
+					Usage: "oslice usage",
+					Flags: []cli.Flag{outFlag, minFlag, maxFlag, lengthFlag},
+					Action: func(c *cli.Context) error {
+						fmt.Println("generate oslice command")
+						fmt.Printf("out flag is set ? %v\n", c.IsSet("out"))
+						fmt.Printf("min flag is set ? %v\n", c.IsSet("min"))
+						fmt.Printf("max flag is set ? %v\n", c.IsSet("max"))
+						fmt.Printf("length flag is set ? %v\n",
+							c.IsSet("length"))
+						return nil
+					},
+				},
+				cli.Command{
+					Name:  "matrix",
+					Usage: "matrix usage",
+					Flags: []cli.Flag{outFlag, minFlag, maxFlag, rowsFlag,
+						columnFlag},
+					Action: func(c *cli.Context) error {
+						fmt.Println("generate matrix command")
+						fmt.Printf("out flag is set ? %v\n", c.IsSet("out"))
+						fmt.Printf("min flag is set ? %v\n", c.IsSet("min"))
+						fmt.Printf("max flag is set ? %v\n", c.IsSet("max"))
+						fmt.Printf("rows flag is set ? %v\n", c.IsSet("rows"))
+						fmt.Printf("cols flag is set ? %v\n",
+							c.IsSet("columns"))
+						return nil
+					},
+				},
+				cli.Command{
+					Name:  "bound",
+					Usage: "bound usage",
+					Flags: []cli.Flag{outFlag, minFlag, maxFlag},
+					Action: func(c *cli.Context) error {
+						fmt.Println("generate bound command")
+						fmt.Printf("out flag is set ? %v\n", c.IsSet("out"))
+						fmt.Printf("min flag is set ? %v\n", c.IsSet("min"))
+						fmt.Printf("max flag is set ? %v\n", c.IsSet("max"))
+						fmt.Printf("rows flag is set ? %v\n", c.IsSet("rows"))
+						return nil
+					},
+				},
+			},
+		},
 	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+	app.Run(os.Args)
 }
