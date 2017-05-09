c778934314910df3b09a764c2e982865c5c31f02
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-09 21:12:45 +0200

    finish Bound implementation

--- samples/bound.go
+++ samples/bound.go
@@ -1,27 +1,34 @@
 package samples
 
+import "fmt"
+
 // TODO add doc
 type Bound struct {
 	lower, upper int64
 }
 
 // TODO add doc
-func NewBound(bLow, bUp int64) (*Bound, error) {
-	if err := checkArgs(bLow, bUp); err != nil {
-		return nil, err
-	}
-
+func NewBound(bLow, bUp int64) *Bound {
 	return &Bound{
 		lower: bLow,
 		upper: bUp,
-	}, nil
-}
-
-func checkArgs(bLow, bUp int64) error {
-
+	}
 }
 
 // TODO add doc
 func (b *Bound) Width() int64 {
 	return b.upper - b.lower
+}
+
+func (b *Bound) Low() int64 {
+	return b.lower
+}
+
+func (b *Bound) Up() int64 {
+	return b.upper
+}
+
+// TODO add doc
+func (b *Bound) String() string {
+	return fmt.Sprintf("(%d %d)", b.lower, b.upper)
 }
