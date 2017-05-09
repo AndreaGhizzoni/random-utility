d0a7e7b7fe402237a2dc8dacc6500973591a90f8
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-09 21:33:46 +0200

    added doc

--- samples/bound.go
+++ samples/bound.go
@@ -2,12 +2,13 @@ package samples
 
 import "fmt"
 
-// TODO add doc
+// Bound structure to represents a bound between a lower and maximum.
 type Bound struct {
 	lower, upper int64
 }
 
-// TODO add doc
+// This function creates a new Bound structure with a lower and upper bound
+// given.
 func NewBound(bLow, bUp int64) *Bound {
 	return &Bound{
 		lower: bLow,
@@ -15,20 +16,22 @@ func NewBound(bLow, bUp int64) *Bound {
 	}
 }
 
-// TODO add doc
+// This function returns the width of the bound.
 func (b *Bound) Width() int64 {
 	return b.upper - b.lower
 }
 
+// This function returns the lower bound of the bound.
 func (b *Bound) Low() int64 {
 	return b.lower
 }
 
+// This function returns the upper bound of the bound.
 func (b *Bound) Up() int64 {
 	return b.upper
 }
 
-// TODO add doc
+// This function returns a string representation of Bound structure.
 func (b *Bound) String() string {
 	return fmt.Sprintf("(%d %d)", b.lower, b.upper)
 }
