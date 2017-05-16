4b03060be83a0e6a7db1eb63b0c84b31f7d3e5e9
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-16 11:12:48 +0200

    create unexported function to generate random slice. added function to generate matrix

--- samples/samples_secure.go
+++ samples/samples_secure.go
@@ -35,8 +35,8 @@ func checkBounds(min, max *big.Int) error {
 	return nil
 }
 
-// TODO add doc
-func (g *SGenerator) generateInt(min, max *big.Int) (*big.Int, error) {
+// TODO add description
+func generateInt(min, max *big.Int) (*big.Int, error) {
 	r, err := rand.Int(rand.Reader, max.Sub(max, min))
 	if err != nil {
 		return nil, err
@@ -45,30 +45,12 @@ func (g *SGenerator) generateInt(min, max *big.Int) (*big.Int, error) {
 
 }
 
-// TODO add doc
-func (g *SGenerator) Int(min, max *big.Int) (*big.Int, error) {
-	if err := checkBounds(min, max); err != nil {
-		return nil, err
-	}
-	return g.generateInt(min, max)
-}
-
-// This function generate a slice of len length, with random numbers X where
-// min <= X < max.
-// If len <= 0 or min > max return a error.
-func (g *SGenerator) Slice_(len, min, max *big.Int) ([]*big.Int, error) {
-	if err := isLessThenZero(len, "Slice length"); err != nil {
-		return nil, err
-	}
-
-	if err := checkBounds(min, max); err != nil {
-		return nil, err
-	}
-
+// TODO add description
+func generateSlice(len, min, max *big.Int) ([]*big.Int, error) {
 	perm := []*big.Int{}
 	i := big.NewInt(0)
 	for ; i.Cmp(len) == -1; i.Add(i, one) {
-		if r, err := g.generateInt(min, max); err != nil {
+		if r, err := generateInt(min, max); err != nil {
 			return nil, err
 		} else {
 			perm = append(perm, r)
@@ -78,36 +60,64 @@ func (g *SGenerator) Slice_(len, min, max *big.Int) ([]*big.Int, error) {
 	return perm, nil
 }
 
-/*
-// This function generate a matrix with r rows and c cols. The number X in it
+// TODO add doc
+func (g *SGenerator) Int(min, max *big.Int) (*big.Int, error) {
+	if err := checkBounds(min, max); err != nil {
+		return nil, err
+	}
+	return generateInt(min, max)
+}
+
+// This function generate a slice of len length, with random numbers X where
+// min <= X < max.
+// If len <= 0 or min > max return a error.
+func (g *SGenerator) Slice(len, min, max *big.Int) ([]*big.Int, error) {
+	if err := isLessThenZero(len, "Slice length"); err != nil {
+		return nil, err
+	}
+
+	if err := checkBounds(min, max); err != nil {
+		return nil, err
+	}
+
+	return generateSlice(len, min, max)
+}
+
+// This function generate a matrix with r rows and c columns. The numbers in it
 // are min <= X < max.
-// If r <= 0 or c <= 0 or min > max, this function return an error
-func (g *Generator) Matrix(r, c, min, max int64) ([][]int64, error) {
+// If r <= 0 or c <= 0 or min => max, this function return an error.
+func (g *SGenerator) Matrix(r, c, min, max *big.Int) ([][]*big.Int, error) {
 	// check rows dimension
-	if err := checkDimension(r, "Matrix rows"); err != nil {
+	if err := isLessThenZero(r, "Matrix rows"); err != nil {
 		return nil, err
 	}
 
 	// check columns too because, in case of error in matrix generation, the
 	// following error message will be displayed: "Slice length given ...", and
-	// is this method do not generate a slice, but a matrix.
-	if err := checkDimension(c, "Matrix columns"); err != nil {
+	// this method do not generate any slice, but a matrix.
+	if err := isLessThenZero(c, "Matrix columns"); err != nil {
 		return nil, err
 	}
 
-	matrix := make([][]int64, r)
-	var i int64 = 0
-	for ; i < r; i++ {
-		perm, e := g.Slice(c, min, max)
+    // check ranges
+	if err := checkBounds(min, max); err != nil {
+		return nil, err
+	}
+
+	matrix := [][]*big.Int{}
+    i := big.NewInt(0)
+	for ; i.Cmp(r) == -1; i.Add(i, one) {
+		perm, e := generateSlice(c, min, max)
 		if e != nil {
 			return nil, e
 		}
-		matrix[i] = perm
+		matrix = append(matrix, perm)
 	}
 
 	return matrix, nil
 }
 
+/*
 // This function generate a random bound of fixed length. min and max are the
 // minimum and the maximum bounds that the bound will be generated of length
 // width.
