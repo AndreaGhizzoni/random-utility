544c18afa0888f22ae62d387ff6eb427e4ab206b
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-16 10:43:49 +0200

    added test for Slice_

--- samples/samples_sgenerator_test.go
+++ samples/samples_sgenerator_test.go
@@ -6,52 +6,90 @@ import (
 	"testing"
 )
 
-func actual_test_SGenerator_Int(t *testing.T, onlyPositive bool) {
-	const max_pow = 64
-	var pow int64 = 1
-	var zero = big.NewInt(0)
+// TODO add description
+func Test_SGenerator_Int(t *testing.T) {
+	zero := big.NewInt(0)
+	one := big.NewInt(1)
+	two := big.NewInt(2)
 
 	// reusable pointer to big.Int
 	var min, max *big.Int = nil, nil
-	if onlyPositive {
-		min = big.NewInt(0)
-	}else{
-		max = big.NewInt(0)
-	}
-
-	gen := samples.NewSecureGenerator()
+	generate := samples.NewSecureGenerator()
 	// pow indicates the power that is used to generate the bounds
-	for ; pow < max_pow; pow++ {
-		d := big.NewInt(2).Exp(big.NewInt(2), big.NewInt(pow), zero)
-		// set max to 2^(pow)
-		if onlyPositive {
-			max = d
-		}else{
-			min = d.Neg(d)
-		}
+	pow := big.NewInt(1)
+	max_pow := big.NewInt(64)
+	for ; pow.Cmp(max_pow) == -1; pow.Add(pow, one) {
+		max = big.NewInt(2).Exp(two, pow, zero)
+		min = big.NewInt(2).Exp(two, pow, zero)
+		min.Neg(min)
 
-		t.Logf("Try to generate secure random number between: min=%v, max=%v",
+		t.Logf("Try to generate secure random number between: min= %v, max= %v",
 			min, max)
 
-		r, err := gen.Int(min, max)
+		r, err := generate.Int(min, max)
 		if err != nil {
 			t.Fatal(err.Error())
 		}
 		t.Logf("Secure random number generated: %v", r)
 
-		if r.Cmp(min) == -1 { // r < tt.min
+		if r.Cmp(min) == -1 { // r < min
 			t.Fatalf("Generated number is less then min: %v < %v", r, min)
 		}
-		if r.Cmp(max) == 1 { // r > tt.max
-			t.Fatalf("Generated number is greater then max: %v > %v", r,
-				max)
+		if r.Cmp(max) == 1 { // r > max
+			t.Fatalf("Generated number is greater then max: %v > %v", r, max)
 		}
 	}
 }
 
-func TestSGenerator_Int(t *testing.T) {
-    // test SGenerator.Int(0, 2**x) where x = {1,2, ..., 64}
-    actual_test_SGenerator_Int(t, true)
-	// test SGenerator.Int(-2**x, 0) where x = {1,2, ..., 64}
-	actual_test_SGenerator_Int(t, false)
+// TODO add description
+func TestSGenerator_Slice_(t *testing.T) {
+	var lengths = []*big.Int{
+		big.NewInt(1),
+		big.NewInt(10),
+		big.NewInt(100),
+		big.NewInt(1000),
+	}
+	zero := big.NewInt(0)
+	one := big.NewInt(1)
+	two := big.NewInt(2)
+
+	// reusable pointer to big.Int
+	var min, max *big.Int = nil, nil
+	var pow *big.Int
+	generate := samples.NewSecureGenerator()
+	// pow indicates the power that is used to generate the bounds
+	max_pow := big.NewInt(64)
+
+	for _, l := range lengths {
+		pow = big.NewInt(1)
+		for ; pow.Cmp(max_pow) == -1; pow.Add(pow, one) {
+			max = big.NewInt(2).Exp(two, pow, zero)
+			min = big.NewInt(2).Exp(two, pow, zero)
+			min.Neg(min)
+
+			t.Logf("Try to generate random secure slice with: l= %v, min= %d,"+
+				" max= %v", l, min, max)
+
+			s, err := generate.Slice_(l, min, max)
+			if err != nil {
+				t.Fatal(err.Error())
+			}
+
+			lenS := int64(len(s))
+			if l.Cmp(big.NewInt(lenS)) != 0 {
+				t.Fatalf("Generated slice length mismatch: %v != %v", l, lenS)
+			}
+
+			for _, e := range s {
+				if e.Cmp(min) == -1 { // e < min
+					t.Fatalf("number in slice is less then min: %v < %v", e,
+						min)
+				}
+				if e.Cmp(max) == 1 { // e > max
+					t.Fatalf("number in slice is greater then max: %v > %v", e,
+						max)
+				}
+			}
+		}
+	}
 }
