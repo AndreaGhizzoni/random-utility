2aa3c979580c6ed4306385d0353fe428da6a7ab1
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-22 10:33:35 +0200

    improved readability of tests

--- samples/samples_sgenerator_test.go
+++ samples/samples_sgenerator_test.go
@@ -7,37 +7,42 @@ import (
 	"testing"
 )
 
+var (
+	modZero = big.NewInt(0)
+	one     = big.NewInt(1)
+	two     = big.NewInt(2)
+)
+
 // TODO add description
 func Test_SGenerator_Int(t *testing.T) {
-	zero := big.NewInt(0)
-	one := big.NewInt(1)
-	two := big.NewInt(2)
-
 	// reusable pointer to big.Int
 	var min, max *big.Int = nil, nil
+	// power indicates the power of 2 that is used to generate the upper and
+	// lower bounds
+	var power *big.Int = nil
+	var max_power = big.NewInt(64)
+
 	generate := samples.NewSecureGenerator()
-	// pow indicates the power that is used to generate the bounds
-	pow := big.NewInt(1)
-	max_pow := big.NewInt(64)
-	for ; pow.Cmp(max_pow) == -1; pow.Add(pow, one) {
-		max = big.NewInt(2).Exp(two, pow, zero)
-		min = big.NewInt(2).Exp(two, pow, zero)
+	for ; power.Cmp(max_power) == -1; power.Add(power, one) {
+		max = big.NewInt(2).Exp(two, power, modZero)
+		min = big.NewInt(2).Exp(two, power, modZero)
 		min.Neg(min)
 
 		t.Logf("Try to generate secure random number between: min= %v, max= %v",
 			min, max)
 
-		r, err := generate.Int(min, max)
+		random, err := generate.Int(min, max)
 		if err != nil {
 			t.Fatal(err.Error())
 		}
-		t.Logf("Secure random number generated: %v", r)
+		t.Logf("Secure random number generated: %v", random)
 
-		if r.Cmp(min) == -1 { // r < min
-			t.Fatalf("Generated number is less then min: %v < %v", r, min)
+		if random.Cmp(min) == -1 { // random < min
+			t.Fatalf("Generated number is less then min: %v < %v", random, min)
 		}
-		if r.Cmp(max) == 1 { // r > max
-			t.Fatalf("Generated number is greater then max: %v > %v", r, max)
+		if random.Cmp(max) == 1 { // random > max
+			t.Fatalf("Generated number is greater then max: %v > %v", random,
+				max)
 		}
 	}
 }
@@ -50,45 +55,44 @@ func TestSGenerator_Slice(t *testing.T) {
 		big.NewInt(100),
 		big.NewInt(1000),
 	}
-	zero := big.NewInt(0)
-	one := big.NewInt(1)
-	two := big.NewInt(2)
 
 	// reusable pointer to big.Int
 	var min, max *big.Int = nil, nil
-	var pow *big.Int
-	generate := samples.NewSecureGenerator()
-	// pow indicates the power that is used to generate the bounds
-	max_pow := big.NewInt(64)
+	// power indicates the power of 2 that is used to generate the upper and
+	// lower bounds
+	var power *big.Int = nil
+	var max_power = big.NewInt(64)
 
-	for _, l := range lengths {
-		pow = big.NewInt(1)
-		for ; pow.Cmp(max_pow) == -1; pow.Add(pow, one) {
-			max = big.NewInt(2).Exp(two, pow, zero)
-			min = big.NewInt(2).Exp(two, pow, zero)
+	generate := samples.NewSecureGenerator()
+	for _, length := range lengths {
+		power = big.NewInt(1)
+		for ; power.Cmp(max_power) == -1; power.Add(power, one) {
+			max = big.NewInt(2).Exp(two, power, modZero)
+			min = big.NewInt(2).Exp(two, power, modZero)
 			min.Neg(min)
 
-			t.Logf("Try to generate random secure slice with: l= %v, min= %v,"+
-				" max= %v", l, min, max)
+			t.Logf("Try to generate random secure slice with: length= %v, "+
+				"min= %v, max= %v", length, min, max)
 
-			s, err := generate.Slice(l, min, max)
+			slice, err := generate.Slice(length, min, max)
 			if err != nil {
 				t.Fatal(err.Error())
 			}
 
-			lenS := int64(len(s))
-			if l.Cmp(big.NewInt(lenS)) != 0 {
-				t.Fatalf("Generated slice length mismatch: %v != %v", l, lenS)
+			actualSliceLength := int64(len(slice))
+			if length.Cmp(big.NewInt(actualSliceLength)) != 0 {
+				t.Fatalf("Generated slice length mismatch: %v != %v", length,
+					actualSliceLength)
 			}
 
-			for _, e := range s {
-				if e.Cmp(min) == -1 { // e < min
-					t.Fatalf("number in slice is less then min: %v < %v", e,
-						min)
+			for _, element := range slice {
+				if element.Cmp(min) == -1 { // element < min
+					t.Fatalf("number in slice is less then min: %v < %v",
+						element, min)
 				}
-				if e.Cmp(max) == 1 { // e > max
-					t.Fatalf("number in slice is greater then max: %v > %v", e,
-						max)
+				if element.Cmp(max) == 1 { // element > max
+					t.Fatalf("number in slice is greater then max: %v > %v",
+						element, max)
 				}
 			}
 		}
@@ -97,54 +101,55 @@ func TestSGenerator_Slice(t *testing.T) {
 
 // TODO add description
 func TestSGenerator_Matrix(t *testing.T) {
-	var lengths = []struct {
+	var matrixDimensions = []struct {
 		r, c *big.Int
 	}{
 		{big.NewInt(1), big.NewInt(1)},
 		{big.NewInt(10), big.NewInt(10)},
 	}
-	zero := big.NewInt(0)
-	one := big.NewInt(1)
-	two := big.NewInt(2)
 
 	// reusable pointer to big.Int
 	var min, max *big.Int = nil, nil
-	var pow *big.Int
+	// power indicates the power of 2 that is used to generate the upper and
+	// lower bounds
+	var power *big.Int = nil
+	var max_power = big.NewInt(64)
+
 	generate := samples.NewSecureGenerator()
-	// pow indicates the power that is used to generate the bounds
-	max_pow := big.NewInt(64)
-	for _, l := range lengths {
-		pow = big.NewInt(1)
-		for ; pow.Cmp(max_pow) == -1; pow.Add(pow, one) {
-			max = big.NewInt(2).Exp(two, pow, zero)
-			min = big.NewInt(2).Exp(two, pow, zero)
+	for _, dimension := range matrixDimensions {
+		power = big.NewInt(1)
+		for ; power.Cmp(max_power) == -1; power.Add(power, one) {
+			max = big.NewInt(2).Exp(two, power, modZero)
+			min = big.NewInt(2).Exp(two, power, modZero)
 			min.Neg(min)
 
 			t.Logf("Try to generate random secure matrix with: r= %v, c= %v, "+
-				"min= %v, max= %v", l.r, l.c, min, max)
+				"min= %v, max= %v", dimension.r, dimension.c, min, max)
 
-			m, err := generate.Matrix(l.r, l.c, min, max)
+			matrix, err := generate.Matrix(dimension.r, dimension.c, min, max)
 			if err != nil {
 				t.Fatal(err.Error())
 			}
 
-			rows := int64(len(m))
-			if l.r.Cmp(big.NewInt(rows)) != 0 {
-				t.Fatalf("Generated matrix rows mismatch: %v != %v", l.r, rows)
+			rows := int64(len(matrix))
+			if dimension.r.Cmp(big.NewInt(rows)) != 0 {
+				t.Fatalf("Generated matrix rows mismatch: %v != %v",
+					dimension.r, rows)
 			}
 
-			cols := int64(len(m[0]))
-			if l.c.Cmp(big.NewInt(cols)) != 0 {
-				t.Fatalf("Generated matrix cols mismatch: %v != %v", l.c, cols)
+			cols := int64(len(matrix[0]))
+			if dimension.c.Cmp(big.NewInt(cols)) != 0 {
+				t.Fatalf("Generated matrix cols mismatch: %v != %v",
+					dimension.c, cols)
 			}
 
-			for _, r := range m {
+			for _, r := range matrix {
 				for _, c := range r {
-					if c.Cmp(min) == -1 { // e < min
+					if c.Cmp(min) == -1 { // c < min
 						t.Fatalf("number in matrix is less then min: %v < "+
 							"%v", c, min)
 					}
-					if c.Cmp(max) == 1 { // e > max
+					if c.Cmp(max) == 1 { // c > max
 						t.Fatalf("number in matrix is greater then max: %v > "+
 							"%v", c, max)
 					}
@@ -156,37 +161,36 @@ func TestSGenerator_Matrix(t *testing.T) {
 
 // TODO add description
 func TestSGenerator_Bound(t *testing.T) {
-	var tableTest = []struct {
-		amount, width *big.Int
-	}{
-		{big.NewInt(10), big.NewInt(1000)},
-		{big.NewInt(100), big.NewInt(1000)},
-		{big.NewInt(1000), big.NewInt(1000)},
+	boundWidth := big.NewInt(1000)
+	var numberOfBounds = []*big.Int{
+		big.NewInt(10),
+		big.NewInt(100),
+		big.NewInt(1000),
 	}
-	zero := big.NewInt(0)
-	one := big.NewInt(1)
-	two := big.NewInt(2)
 
 	// reusable pointer to big.Int
 	var min, max *big.Int = nil, nil
-	var pow *big.Int
+	// power indicates the power of 2 that is used to generate the upper and
+	// lower bounds
+	var power *big.Int = nil
+	var max_power = big.NewInt(64)
+
 	generate := samples.NewSecureGenerator()
-	// pow indicates the power that is used to generate the bounds
-	max_pow := big.NewInt(64)
-	for _, l := range tableTest {
-		pow = big.NewInt(10) // power start to 10 because of fixed bound width
-		for ; pow.Cmp(max_pow) == -1; pow.Add(pow, one) {
-			max = big.NewInt(2).Exp(two, pow, zero)
-			min = big.NewInt(2).Exp(two, pow, zero)
+	for _, amount := range numberOfBounds {
+		// It starts at 10 because of fixed boundWidth
+		power = big.NewInt(10)
+		for ; power.Cmp(max_power) == -1; power.Add(power, one) {
+			max = big.NewInt(2).Exp(two, power, modZero)
+			min = big.NewInt(2).Exp(two, power, modZero)
 			min.Neg(min)
 
-			t.Logf("Try to generate random secure bound with: a= %v, w= %v, "+
-				"min= %v, max= %v", l.amount, l.width, min, max)
+			t.Logf("Try to generate random secure bound with: amount= %v, "+
+				"w= %v, min= %v, max= %v", amount, boundWidth, min, max)
 
 			bounds := []*structures.Bound{}
 			i := big.NewInt(0)
-			for ; i.Cmp(l.amount) == -1; i.Add(i, one) {
-				b, err := generate.Bound(min, max, l.width)
+			for ; i.Cmp(amount) == -1; i.Add(i, one) {
+				b, err := generate.Bound(min, max, boundWidth)
 				if err != nil {
 					t.Fatal(err.Error())
 				}
@@ -194,23 +198,23 @@ func TestSGenerator_Bound(t *testing.T) {
 			}
 
 			lenBounds := int64(len(bounds))
-			if l.amount.Cmp(big.NewInt(lenBounds)) != 0 {
+			if amount.Cmp(big.NewInt(lenBounds)) != 0 {
 				t.Fatalf("Generated amount of bounds mismatch: %v != %v",
-					l.amount, lenBounds)
+					amount, lenBounds)
 			}
 
-			for _, b := range bounds {
-				if b.Low().Cmp(min) == -1 { // lower bound < min
+			for _, bound := range bounds {
+				if bound.Low().Cmp(min) == -1 { // lower bound < min
 					t.Fatalf("lower bound is less then min: %v < %v",
-						b.Low(), min)
+						bound.Low(), min)
 				}
-				if b.Up().Cmp(max) == 1 { // upper bound > max
+				if bound.Up().Cmp(max) == 1 { // upper bound > max
 					t.Fatalf("upper bound is greater then max: %v > %v",
-						b.Up(), max)
+						bound.Up(), max)
 				}
-				if b.Width().Cmp(l.width) != 0 { // bound width != width
-					t.Fatalf("bound width mismatch: %v != %v",
-						b.Width(), l.width)
+				if bound.Width().Cmp(boundWidth) != 0 { // width != boundWidth
+					t.Fatalf("bound boundWidth mismatch: %v != %v",
+						bound.Width(), boundWidth)
 				}
 			}
 		}
