50e537aa1e74fd45f5b3e24e7ae6adc887d75a1b
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-22 22:26:19 +0200

    update comments, now they follow the standards

--- structures/bound.go
+++ structures/bound.go
@@ -10,32 +10,30 @@ type Bound struct {
 	lower, upper *big.Int
 }
 
-// This function creates a new Bound structure with a lower and upper bound
-// given.
+// NewBound creates a new Bound structure with a lower and upper bound given.
 func NewBound(bLow, bUp *big.Int) *Bound {
-	// TODO check bounds
 	return &Bound{
 		lower: bLow,
 		upper: bUp,
 	}
 }
 
-// This function returns the width of the bound.
+// Width returns the width of the bound.
 func (b *Bound) Width() *big.Int {
 	return big.NewInt(0).Sub(b.upper, b.lower)
 }
 
-// This function returns the lower bound of the bound.
+// Low returns the lower bound of the bound.
 func (b *Bound) Low() *big.Int {
 	return b.lower
 }
 
-// This function returns the upper bound of the bound.
+// Up returns the upper bound of the bound.
 func (b *Bound) Up() *big.Int {
 	return b.upper
 }
 
-// This function returns a string representation of Bound structure.
+// String returns a string representation of Bound structure.
 func (b *Bound) String() string {
 	return fmt.Sprintf("(%v %v)", b.lower, b.upper)
 }
