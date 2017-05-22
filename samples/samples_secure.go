62d3b3edd2deafbb963da117193608585330b3ee
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-22 11:04:44 +0200

    modified SGenerator.Bound into SGenerator.Bounds that returns []*structure.Bound

--- samples/samples_secure.go
+++ samples/samples_secure.go
@@ -2,7 +2,6 @@ package samples
 
 import (
 	"crypto/rand"
-	"errors"
 	"github.com/AndreaGhizzoni/zenium/structures"
 	"github.com/AndreaGhizzoni/zenium/util"
 	"math/big"
@@ -10,7 +9,6 @@ import (
 
 // TODO add doc
 type SGenerator struct{}
-
 
 // TODO add doc
 func NewSecureGenerator() *SGenerator {
@@ -31,7 +29,7 @@ func generateInt(min, max *big.Int) (*big.Int, error) {
 func generateSlice(len, min, max *big.Int) ([]*big.Int, error) {
 	perm := []*big.Int{}
 	i := big.NewInt(0)
-	for ; i.Cmp(len) == -1; i.Add(i, one) {
+	for ; i.Cmp(len) == -1; i.Add(i, util.One) {
 		if r, err := generateInt(min, max); err != nil {
 			return nil, err
 		} else {
@@ -40,6 +38,24 @@ func generateSlice(len, min, max *big.Int) ([]*big.Int, error) {
 	}
 
 	return perm, nil
+}
+
+// TODO add description
+func generateBound(min, max, width *big.Int) (*structures.Bound, error) {
+	bLow, err := generateInt(min, max)
+	if err != nil {
+		return nil, err
+	}
+	bUp := big.NewInt(0)
+
+	bLowPlusWidth := big.NewInt(0).Add(bLow, width)
+	if bLowPlusWidth.Cmp(max) == 1 { // bLow+width > max
+		bUp = max
+		bLow.Sub(bUp, width) //bLow = bUp - width
+	} else {
+		bUp.Add(bLow, width)
+	}
+	return structures.NewBound(bLow, bUp), nil
 }
 
 // TODO add doc
@@ -88,7 +104,7 @@ func (g *SGenerator) Matrix(r, c, min, max *big.Int) ([][]*big.Int, error) {
 
 	matrix := [][]*big.Int{}
 	i := big.NewInt(0)
-	for ; i.Cmp(r) == -1; i.Add(i, one) {
+	for ; i.Cmp(r) == -1; i.Add(i, util.One) {
 		perm, e := generateSlice(c, min, max)
 		if e != nil {
 			return nil, e
@@ -99,30 +115,27 @@ func (g *SGenerator) Matrix(r, c, min, max *big.Int) ([][]*big.Int, error) {
 	return matrix, nil
 }
 
-// This function generate a random bound of fixed length. min and max are the
-// minimum and the maximum bounds that the bound will be generated of length
-// width.
-func (g *SGenerator) Bound(min, max, width *big.Int) (*structures.Bound, error) {
-	if err := util.IsLessThenOne(width, "Bound with"); err != nil {
+// This function generate a slice of random structure.Bound.
+// min and max represents the lower and the upprer bounds of element's slice.
+// width is the fixed with of all the bounds.
+// amount is the number of bounds that will be generated.
+func (g *SGenerator) Bounds(min, max, width, amount *big.Int) ([]*structures.Bound, error) {
+	if err := util.IsLessThenOne(width, "Bound width"); err != nil {
 		return nil, err
 	}
+	// TODO check if (max-min >= width)
 
 	if err := util.CheckBounds(min, max); err != nil {
 		return nil, err
 	}
 
-	bLow, err := generateInt(min, max)
-	if err != nil {
-		return nil, err
+	boundSlice := make([]*structures.Bound, amount.Int64())
+	i := big.NewInt(0)
+	for ; i.Cmp(amount) == -1; i.Add(i, util.One) {
+		if bound, err := g.Bound(min, max, boundWidth); err != nil {
+			return nil, err
+		} else {
+			boundSlice = append(boundSlice, bound)
+		}
 	}
-	bUp := big.NewInt(0)
-
-	i := big.NewInt(0).Add(bLow, width)
-	if i.Cmp(max) == 1 { // bLow+width > max
-		bUp = max
-		bLow.Sub(bUp, width) //bLow = bUp - width
-	} else {
-		bUp.Add(bLow, width)
-	}
-	return structures.NewBound(bLow, bUp), nil
 }
