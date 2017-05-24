c202d9c3ed58d0e38a6e0f8347fa839ae31d2760
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-24 09:04:19 +0200

    refactoring comments and make code more clear

--- out/out.go
+++ out/out.go
@@ -7,16 +7,15 @@ import (
 	"os"
 )
 
-// This structure is responsible to know how to write custom structure to file.
-// Every WriteX method, where X is a structure, produce a single file.
+// Printer is responsible to know how to write custom structure to file or
+// console. Every Printer.WriteX method, where X is a structure, produce
+// representation of X in format specified in README.md.
 type Printer struct {
 	file *os.File
 }
 
-// This method instance a new printer that knows how to write some data
-// structures to given path, according to README.md file.
-// Argument path must be a file path (relative/absolute) or empty string to
-// print on standard out.
+// NewPrinter instance a new Printer. Argument path must be a file path
+// (relative/absolute) or empty string to print on standard out.
 // error is returned if: path leads to a non-readable/non-writable file or
 // leads to a directory
 func NewPrinter(path string) (*Printer, error) {
@@ -34,7 +33,7 @@ func NewPrinter(path string) (*Printer, error) {
 	return &Printer{os.Stdout}, nil
 }
 
-// reusable method to write a single slice on given file
+// reusable method to write a single slice
 func (p *Printer) writeSingleSlice(slice []int64) error {
 	var s string
 	for _, v := range slice {
@@ -47,95 +46,88 @@ func (p *Printer) writeSingleSlice(slice []int64) error {
 	return err
 }
 
-// This method write a given slice according to instanced out.Printer.
+// reusable method to check write synchronization.
+func (p *Printer) synchronizeFileWrite () error{
+	if err := p.file.Sync(); err != nil {
+		return err
+	}
+    return nil
+}
+
+// WriteSlice write the given slice in specified format according to instanced
+// out.Printer.
 // error is returned if: slice == nil or there is an i/o error.
 func (p *Printer) WriteSlice(slice []int64) error {
 	if slice == nil {
-		return errors.New("Given slice can not be nil")
+		return errors.New("Given slice can not be nil.")
 	}
 
-	// write header for slice. Check README
-	s := fmt.Sprintf("%d\n", len(slice))
-	if _, err := p.file.WriteString(s); err != nil {
+	sliceHeader := fmt.Sprintf("%d\n", len(slice))
+	if _, err := p.file.WriteString(sliceHeader); err != nil {
 		return err
 	}
-	// write slice
+
 	if err := p.writeSingleSlice(slice); err != nil {
 		return err
 	}
 
-	// Issue a Sync to flush writes to stable storage
-	if err := p.file.Sync(); err != nil {
-		return err
-	}
-	return nil
+	return p.synchronizeFileWrite()
 }
 
-// This method write a given matrix according to instanced out.Printer.
-// error is returned if: matrix == nil or there is an i/o error.
+// WriteMatrix write a given matrix in specified format according to instanced
+// out.Printer.
 // This method assumes that every rows in the matrix has the same number of
-// elements of matrix[0]
+// elements of matrix[0].
+// error is returned if: matrix == nil or there is an i/o error.
 func (p *Printer) WriteMatrix(matrix [][]int64) error {
 	if matrix == nil {
-		return errors.New("Given matrix can not be nil")
+		return errors.New("Given matrix can not be nil.")
 	}
 
-	// write header for matrix. Check README
-	// check matrix has at least one row
 	if rows := len(matrix); rows != 0 {
-		s := fmt.Sprintf("%d %d\n", rows, len(matrix[0]))
-		if _, err := p.file.WriteString(s); err != nil {
+		matrixHeader := fmt.Sprintf("%d %d\n", rows, len(matrix[0]))
+		if _, err := p.file.WriteString(matrixHeader); err != nil {
 			return err
 		}
 	} else {
-		return errors.New("Given Matrix has zero rows")
+		return errors.New("Given matrix has zero rows.")
 	}
 
-	// write every matrix row into the file
-	for _, slice := range matrix {
-		if err := p.writeSingleSlice(slice); err != nil {
+	for _, rows := range matrix {
+		if err := p.writeSingleSlice(rows); err != nil {
 			return err
 		}
 	}
 
-	// Issue a Sync to flush writes to stable storage
-	if err := p.file.Sync(); err != nil {
-		return err
-	}
-	return nil
+	return p.synchronizeFileWrite()
 }
 
-// This method writes a single sample.Bound structure according to instanced
-// out.Printer. error is returned if there is a i/o error.
+// WriteBound writes a single sample.Bound structure in specified format
+// according to instanced out.Printer. error is returned if there is a i/o error.
 func (p *Printer) WriteBound(bound samples.Bound) error {
 	return p.WriteBounds([]samples.Bound{bound})
 }
 
-// This method writes a slice of sample.Bound according to instanced
-// out.Printer. error is returned if there is a i/o error or if slice is nil.
+// WriteBounds writes a slice of sample.Bound in specified format according to
+// instanced out.Printer.
+// error is returned if there is a i/o error or if slice is nil.
 func (p *Printer) WriteBounds(bounds []samples.Bound) error {
 	if bounds == nil {
 		return errors.New("Given bound can not be nil")
 	}
 
-	// write header for bounds. Check README
-	header := fmt.Sprintf("%d\n", len(bounds))
-	if _, err := p.file.WriteString(header); err != nil {
+	boundHeader := fmt.Sprintf("%d\n", len(bounds))
+	if _, err := p.file.WriteString(boundHeader); err != nil {
 		return err
 	}
 
-	// for every bound write it into file
-	var row string = ""
+	var boundAsString string
 	for _, b := range bounds {
-		row = fmt.Sprintf("%d %d\n", b.Low(), b.Up())
-		if _, err := p.file.WriteString(row); err != nil {
+		boundAsString = fmt.Sprintf("%d %d\n", b.Low(), b.Up())
+		if _, err := p.file.WriteString(boundAsString); err != nil {
 			return err
 		}
 	}
 
-	// Issue a Sync to flush writes to stable storage
-	if err := p.file.Sync(); err != nil {
-		return err
-	}
-	return nil
+	return p.synchronizeFileWrite()
 }
