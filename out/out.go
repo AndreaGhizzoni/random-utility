c11b69525d4d9ffdabbd97606a7ecf2a918c4747
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-27 10:47:48 +0200

    added new printer that works with big.Int

--- out/out.go
+++ out/out.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"github.com/AndreaGhizzoni/zenium/samples"
+	"github.com/AndreaGhizzoni/zenium/structures"
+	"math/big"
 	"os"
 )
 
@@ -11,6 +13,11 @@ import (
 // console. Every Printer.WriteX method, where X is a structure, produce
 // representation of X in format specified in README.md.
 type Printer struct {
+	file *os.File
+}
+
+// TODO add doc
+type SPrinter struct{
 	file *os.File
 }
 
@@ -33,6 +40,22 @@ func NewPrinter(path string) (*Printer, error) {
 	return &Printer{os.Stdout}, nil
 }
 
+// TODO add doc
+func NewSecurePrinter(path string) (*SPrinter, error) {
+	if path != "" {
+		dir, file, errS := sanitizePath(path)
+		if errS != nil {
+			return nil, errS
+		}
+		oFile, errF := openFileIfCanRW(dir, file)
+		if errF != nil {
+			return nil, errF
+		}
+		return &SPrinter{oFile}, nil
+	}
+	return &SPrinter{os.Stdout}, nil
+}
+
 // reusable method to write a single slice
 func (p *Printer) writeSingleSlice(slice []int64) error {
 	var s string
@@ -46,18 +69,57 @@ func (p *Printer) writeSingleSlice(slice []int64) error {
 	return err
 }
 
+// TODO add description
+func (p *SPrinter) writeSingleSlice(slice []*big.Int) error {
+	var stringOfElement string
+	for _, element := range slice {
+		stringOfElement = fmt.Sprintf("%v ", element)
+		if _, err := p.file.WriteString(stringOfElement); err != nil {
+			return err
+		}
+	}
+	_, err := p.file.WriteString("\n")
+	return err
+}
+
 // reusable method to check write synchronization.
-func (p *Printer) synchronizeFileWrite () error{
+func (p *Printer) synchronizeFileWrite() error {
 	if err := p.file.Sync(); err != nil {
 		return err
 	}
-    return nil
+	return nil
+}
+
+// reusable method to check write synchronization.
+func (p *SPrinter) synchronizeFileWrite() error {
+	if err := p.file.Sync(); err != nil {
+		return err
+	}
+	return nil
 }
 
 // WriteSlice write the given slice in specified format according to instanced
 // out.Printer.
 // error is returned if: slice == nil or there is an i/o error.
 func (p *Printer) WriteSlice(slice []int64) error {
+	if slice == nil {
+		return errors.New("Given slice can not be nil.")
+	}
+
+	sliceHeader := fmt.Sprintf("%d\n", len(slice))
+	if _, err := p.file.WriteString(sliceHeader); err != nil {
+		return err
+	}
+
+	if err := p.writeSingleSlice(slice); err != nil {
+		return err
+	}
+
+	return p.synchronizeFileWrite()
+}
+
+// TODO add description
+func (p *SPrinter) WriteSlice(slice []*big.Int) error {
 	if slice == nil {
 		return errors.New("Given slice can not be nil.")
 	}
@@ -102,10 +164,39 @@ func (p *Printer) WriteMatrix(matrix [][]int64) error {
 	return p.synchronizeFileWrite()
 }
 
+// TODO add description
+func (p *SPrinter) WriteMatrix(matrix [][]*big.Int) error {
+	if matrix == nil {
+		return errors.New("Given matrix can not be nil.")
+	}
+
+	if rows := len(matrix); rows != 0 {
+		matrixHeader := fmt.Sprintf("%d %d\n", rows, len(matrix[0]))
+		if _, err := p.file.WriteString(matrixHeader); err != nil {
+			return err
+		}
+	} else {
+		return errors.New("Given matrix has zero rows.")
+	}
+
+	for _, rows := range matrix {
+		if err := p.writeSingleSlice(rows); err != nil {
+			return err
+		}
+	}
+
+	return p.synchronizeFileWrite()
+}
+
 // WriteBound writes a single sample.Bound structure in specified format
 // according to instanced out.Printer. error is returned if there is a i/o error.
 func (p *Printer) WriteBound(bound samples.Bound) error {
 	return p.WriteBounds([]samples.Bound{bound})
+}
+
+// TODO add description
+func (p *SPrinter) WriteBound(bound *structures.Bound) error {
+	return p.WriteBounds([]*structures.Bound{bound})
 }
 
 // WriteBounds writes a slice of sample.Bound in specified format according to
@@ -124,6 +215,28 @@ func (p *Printer) WriteBounds(bounds []samples.Bound) error {
 	var boundAsString string
 	for _, b := range bounds {
 		boundAsString = fmt.Sprintf("%d %d\n", b.Low(), b.Up())
+		if _, err := p.file.WriteString(boundAsString); err != nil {
+			return err
+		}
+	}
+
+	return p.synchronizeFileWrite()
+}
+
+// TODO add description
+func (p *SPrinter) WriteBounds(bounds []*structures.Bound) error {
+	if bounds == nil {
+		return errors.New("Given bound can not be nil")
+	}
+
+	boundHeader := fmt.Sprintf("%d\n", len(bounds))
+	if _, err := p.file.WriteString(boundHeader); err != nil {
+		return err
+	}
+
+	var boundAsString string
+	for _, b := range bounds {
+		boundAsString = fmt.Sprintf("%v %v\n", b.Low(), b.Up())
 		if _, err := p.file.WriteString(boundAsString); err != nil {
 			return err
 		}
