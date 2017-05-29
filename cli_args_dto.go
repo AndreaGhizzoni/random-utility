97b8c50ab5436ccda418073905e8a48e0af988c7
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-29 18:19:05 +0200

    testing new dto

--- cli_args_dto.go
+++ cli_args_dto.go
@@ -13,45 +13,70 @@ type cliArgsDTO struct {
 	Amount                       *big.Int
 }
 
-func NewCliArgsDTO(c *cli.Context) (*cliArgsDTO, error) {
-	output := c.String("out")
-	stringLength := c.String("length")
-	stringMin := c.String("min")
-	stringMax := c.String("max")
-	stringRows := c.String("rows")
-	stringCols := c.String("columns")
-	stringWidth := c.String("width")
-	stringAmount := c.String("amount")
-	var err error
-
+func NewCliArgsDTO_(context *cli.Context) (*cliArgsDTO, error) {
 	dto := new(cliArgsDTO)
+	for _, flagName := range context.FlagNames() {
+		if flagName == "out" {
+			dto.OutPath = context.String("out")
+		} else {
+			bigIntOfString, err := util.FromStringToBigInt(flagName, flagName)
+			if err != nil {
+				return nil, err
+			}
+			dto.placeInCorrectField(flagName, bigIntOfString)
+		}
+	}
+}
 
-	dto.OutPath = output
-	dto.Length, err = util.FromStringToBigInt(stringLength, "Length")
+func (dto *cliArgsDTO) placeInCorrectField(fieldName string, value *big.Int) {
+	switch fieldName {
+	case "length":
+		dto.Length = value
+	case "min":
+		dto.Min = value
+	case "max":
+		dto.Max = value
+	case "rows":
+		dto.Rows = value
+	case "columns":
+		dto.Columns = value
+	case "width":
+		dto.Width = value
+	case "amount":
+		dto.Amount = value
+	}
+}
+
+func NewCliArgsDTO(c *cli.Context) (*cliArgsDTO, error) {
+	var err error
+	dto := new(cliArgsDTO)
+	dto.OutPath = c.String("out")
+
+	dto.Length, err = util.FromStringToBigInt(c.String("length"), "Length")
 	if err != nil {
 		return nil, err
 	}
-	dto.Min, err = util.FromStringToBigInt(stringMin, "Minimum")
+	dto.Min, err = util.FromStringToBigInt(c.String("min"), "Minimum")
 	if err != nil {
 		return nil, err
 	}
-	dto.Max, err = util.FromStringToBigInt(stringMax, "Maximum")
+	dto.Max, err = util.FromStringToBigInt(c.String("max"), "Maximum")
 	if err != nil {
 		return nil, err
 	}
-	dto.Rows, err = util.FromStringToBigInt(stringRows, "Rows")
+	dto.Rows, err = util.FromStringToBigInt(c.String("rows"), "Rows")
 	if err != nil {
 		return nil, err
 	}
-	dto.Columns, err = util.FromStringToBigInt(stringCols, "Columns")
+	dto.Columns, err = util.FromStringToBigInt(c.String("columns"), "Columns")
 	if err != nil {
 		return nil, err
 	}
-	dto.Width, err = util.FromStringToBigInt(stringWidth, "Width")
+	dto.Width, err = util.FromStringToBigInt(c.String("width"), "Width")
 	if err != nil {
 		return nil, err
 	}
-	dto.Amount, err = util.FromStringToBigInt(stringAmount, "Amount")
+	dto.Amount, err = util.FromStringToBigInt(c.String("amount"), "Amount")
 	if err != nil {
 		return nil, err
 	}
