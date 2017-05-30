77ec3c72c6476c9a3ca6a519ed6e8c4c3294977c
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-05-30 08:30:25 +0200

    reducing dto complexity

--- cli_args_dto.go
+++ cli_args_dto.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/AndreaGhizzoni/zenium/util"
 	"github.com/urfave/cli"
 	"math/big"
@@ -13,19 +14,23 @@ type cliArgsDTO struct {
 	Amount                       *big.Int
 }
 
-func NewCliArgsDTO_(context *cli.Context) (*cliArgsDTO, error) {
+func NewCliArgsDTO(context *cli.Context) (*cliArgsDTO, error) {
 	dto := new(cliArgsDTO)
 	for _, flagName := range context.FlagNames() {
 		if flagName == "out" {
 			dto.OutPath = context.String("out")
 		} else {
-			bigIntOfString, err := util.FromStringToBigInt(flagName, flagName)
+			bigIntOfString, err := util.FromStringToBigInt(
+				context.String(flagName),
+				flagName,
+			)
 			if err != nil {
 				return nil, err
 			}
 			dto.placeInCorrectField(flagName, bigIntOfString)
 		}
 	}
+	return dto, nil
 }
 
 func (dto *cliArgsDTO) placeInCorrectField(fieldName string, value *big.Int) {
@@ -45,40 +50,4 @@ func (dto *cliArgsDTO) placeInCorrectField(fieldName string, value *big.Int) {
 	case "amount":
 		dto.Amount = value
 	}
-}
-
-func NewCliArgsDTO(c *cli.Context) (*cliArgsDTO, error) {
-	var err error
-	dto := new(cliArgsDTO)
-	dto.OutPath = c.String("out")
-
-	dto.Length, err = util.FromStringToBigInt(c.String("length"), "Length")
-	if err != nil {
-		return nil, err
-	}
-	dto.Min, err = util.FromStringToBigInt(c.String("min"), "Minimum")
-	if err != nil {
-		return nil, err
-	}
-	dto.Max, err = util.FromStringToBigInt(c.String("max"), "Maximum")
-	if err != nil {
-		return nil, err
-	}
-	dto.Rows, err = util.FromStringToBigInt(c.String("rows"), "Rows")
-	if err != nil {
-		return nil, err
-	}
-	dto.Columns, err = util.FromStringToBigInt(c.String("columns"), "Columns")
-	if err != nil {
-		return nil, err
-	}
-	dto.Width, err = util.FromStringToBigInt(c.String("width"), "Width")
-	if err != nil {
-		return nil, err
-	}
-	dto.Amount, err = util.FromStringToBigInt(c.String("amount"), "Amount")
-	if err != nil {
-		return nil, err
-	}
-	return dto, nil
 }
